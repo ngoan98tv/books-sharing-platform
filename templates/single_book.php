32012d73115b35e3db5092692024414e3afc4aad
ngoan98tv <ngoan98travinh@gmail.com>
2019-05-01 00:14:30 +0700

    update single book style

--- templates/single_book.php
+++ templates/single_book.php
@@ -2,10 +2,14 @@
 function show_a_book($book) {
 echo <<<_SINGLE_BOOK_TEMPLATE
     <div class='single-book'>
+        <div>
         <img src='$book[cover_url]'>
         <p class='title'>$book[title]</p>
         <p class='author'>$book[author]</p>
         <p class='year'>$book[year]</p>
+        <p class='hidden'>$book[name]</p>
+        <button class='hidden'>Download</button>
+        </div>
     </div>
 _SINGLE_BOOK_TEMPLATE;
 }
