1ddc80239af06a2c568d1bd57e0f088014a6b5b9
ngoan98tv <ngoan98travinh@gmail.com>
2019-12-01 16:28:11 +0700

    update db

--- database/books_sample_data.sql
+++ database/books_sample_data.sql
@@ -36,7 +36,7 @@ CREATE TABLE `book` (
   KEY `fk_book_category1_idx` (`category_id`),
   CONSTRAINT `fk_book_category1` FOREIGN KEY (`category_id`) REFERENCES `category` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_book_uploader` FOREIGN KEY (`uploader_id`) REFERENCES `uploader` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=103 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=104 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -45,7 +45,7 @@ CREATE TABLE `book` (
 
 LOCK TABLES `book` WRITE;
 /*!40000 ALTER TABLE `book` DISABLE KEYS */;
-INSERT INTO `book` VALUES (82,'C Programming Language','Jack Edwina',2000,'uploads/images/10-C-Programming-Language.jpg',10,5,'uploads/files/10-baitapon.pdf'),(83,'Lap trinh Java','Richard Grimmett',2000,'uploads/images/10-javaforbengginer.jpg',10,5,'uploads/files/10-baitapon.pdf'),(84,'Fantastic Code In Bath Room','Jack Edwina',2011,'uploads/images/10-9781138626706.jpg',10,6,'uploads/files/10-baitapon.pdf'),(85,'Fantastic Code In Bath Room','Jack Edwina',2000,'uploads/images/14-3081cd96e225007b5934.jpg',14,6,'uploads/files/14-baitapon.pdf'),(86,'Lap trinh Java','Richard Grimmett',2000,'uploads/images/11-javaforbengginer.jpg',11,6,'uploads/files/11-baitapon.pdf'),(87,'Lap trinh Java','Richard Grimmett',2000,'uploads/images/11-Introduction-to-Combinatorics.jpg',11,5,'uploads/files/11-bi-tp-chng-2.pdf'),(88,'Lap trinh Java','Richard Grimmett',2000,'uploads/images/11-C-Programming-Language.jpg',11,5,'uploads/files/11-baitapon.pdf'),(89,'C Programming Language','Ngoan Tran',2000,'uploads/images/11-9781138626706.jpg',11,5,'uploads/files/11-baitapon.pdf'),(90,'C Programming Language','Jack Edwina',0000,'uploads/images/11-Discrete-Mathematics-and-Its-Applications-Seventh-Edition.jpg',11,7,'uploads/files/11-baitapon.pdf'),(91,'C Programming Language','Richard Grimmett',2000,'uploads/images/11-javaforbengginer.jpg',11,7,'uploads/files/11-bi-tp-chng-2.pdf'),(92,'C Programming Language','Richard Grimmett',0000,'uploads/images/11-9781138626706.jpg',11,7,'uploads/files/11-baitapon.pdf'),(93,'C Programming Language','Maja Olejniczak',2000,'uploads/images/11-bai-tap-box-model-buoc-1.png',11,7,'uploads/files/11-baitapon.pdf'),(94,'Lap trinh Java','Richard Grimmett',2000,'uploads/images/11-Introduction-to-Combinatorics.jpg',11,6,'uploads/files/11-baitapon.pdf'),(95,'Lap trinh Java','Jack Edwina',2000,'uploads/images/10-Discrete-Mathematics-and-Its-Applications-Seventh-Edition.jpg',10,7,'uploads/files/10-baitapon.pdf'),(96,'Lap trinh Java','Jack Edwina',2000,'uploads/images/10-C-Programming-Language.jpg',10,7,'uploads/files/10-baitapon.pdf'),(97,'Lap trinh Java','Maja Olejniczak',2000,'uploads/images/10-9781138626706.jpg',10,7,'uploads/files/10-baitapon.pdf'),(98,'C Programming Language','Richard Grimmett',2000,'uploads/images/10-javaforbengginer.jpg',10,7,'uploads/files/10-baitapon.pdf'),(99,'Lap trinh Java','Richard Grimmett',2000,'uploads/images/10-Discrete-Mathematics-and-Its-Applications-Seventh-Edition.jpg',10,7,'uploads/files/10-baitapon.pdf'),(100,'C Programming Language','Maja Olejniczak',2000,'uploads/images/10-C-Programming-Language.jpg',10,7,'uploads/files/10-baitapon.pdf'),(101,'C Programming Language','Maja Olejniczak',2000,'uploads/images/10-Introduction-to-Combinatorics.jpg',10,7,'uploads/files/10-baitapon.pdf'),(102,'Lap trinh Java','Jack Edwina',2000,'uploads/images/10-Discrete-Mathematics-and-Its-Applications-Seventh-Edition.jpg',10,7,'uploads/files/10-baitapon.pdf');
+INSERT INTO `book` VALUES (82,'C Programming Language','Jack Edwina',2000,'uploads/images/10-C-Programming-Language.jpg',10,5,'uploads/files/10-baitapon.pdf'),(83,'Lap trinh Java','Richard Grimmett',2000,'uploads/images/10-javaforbengginer.jpg',10,5,'uploads/files/10-baitapon.pdf'),(84,'Fantastic Code In Bath Room','Jack Edwina',2011,'uploads/images/10-9781138626706.jpg',10,6,'uploads/files/10-baitapon.pdf'),(85,'Fantastic Code In Bath Room','Jack Edwina',2000,'uploads/images/14-3081cd96e225007b5934.jpg',14,6,'uploads/files/14-baitapon.pdf'),(86,'Lap trinh Java','Richard Grimmett',2000,'uploads/images/11-javaforbengginer.jpg',11,6,'uploads/files/11-baitapon.pdf'),(87,'Lap trinh Java','Richard Grimmett',2000,'uploads/images/11-Introduction-to-Combinatorics.jpg',11,5,'uploads/files/11-bi-tp-chng-2.pdf'),(88,'Lap trinh Java','Richard Grimmett',2000,'uploads/images/11-C-Programming-Language.jpg',11,5,'uploads/files/11-baitapon.pdf'),(89,'C Programming Language','Ngoan Tran',2000,'uploads/images/11-9781138626706.jpg',11,5,'uploads/files/11-baitapon.pdf'),(90,'C Programming Language','Jack Edwina',0000,'uploads/images/11-Discrete-Mathematics-and-Its-Applications-Seventh-Edition.jpg',11,7,'uploads/files/11-baitapon.pdf'),(91,'C Programming Language','Richard Grimmett',2000,'uploads/images/11-javaforbengginer.jpg',11,7,'uploads/files/11-bi-tp-chng-2.pdf'),(92,'C Programming Language','Richard Grimmett',0000,'uploads/images/11-9781138626706.jpg',11,7,'uploads/files/11-baitapon.pdf'),(93,'C Programming Language','Maja Olejniczak',2000,'uploads/images/11-bai-tap-box-model-buoc-1.png',11,7,'uploads/files/11-baitapon.pdf'),(94,'Lap trinh Java111','Richard Grimmett',2000,'uploads/images/11-C-Programming-Language.jpg',11,6,'uploads/files/11-baitapon.pdf'),(95,'Lap trinh Java','Jack Edwina',2000,'uploads/images/10-Introduction-to-Combinatorics.jpg',10,7,'uploads/files/10-baitapon.pdf'),(96,'Lap trinh Java','Jack Edwina',2000,'uploads/images/10-C-Programming-Language.jpg',10,7,'uploads/files/10-baitapon.pdf'),(97,'Lap trinh Java','Maja Olejniczak',2000,'uploads/images/10-9781138626706.jpg',10,7,'uploads/files/10-baitapon.pdf'),(98,'C Programming Language','Richard Grimmett',2000,'uploads/images/10-javaforbengginer.jpg',10,7,'uploads/files/10-baitapon.pdf'),(99,'Lap trinh Java','Richard Grimmett',2000,'uploads/images/10-javaforbengginer.jpg',10,7,'uploads/files/10-baitapon.pdf'),(100,'C Programming Language','Maja Olejniczak',2000,'uploads/images/10-C-Programming-Language.jpg',10,7,'uploads/files/10-baitapon.pdf'),(101,'C Programming Language','Maja Olejniczak',2000,'uploads/images/10-C-Programming-Language.jpg',10,7,'uploads/files/10-baitapon.pdf'),(102,'Lap trinh Java','Jack Edwina',2000,'uploads/images/10-javaforbengginer.jpg',10,7,'uploads/files/10-baitapon.pdf'),(103,'Lap trinh Java','Jack Edwina',2000,'uploads/images/11-C-Programming-Language.jpg',11,6,'uploads/files/11-baitapon.pdf');
 /*!40000 ALTER TABLE `book` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -90,7 +90,7 @@ CREATE TABLE `uploader` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `username_UNIQUE` (`username`),
   UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -99,7 +99,7 @@ CREATE TABLE `uploader` (
 
 LOCK TABLES `uploader` WRITE;
 /*!40000 ALTER TABLE `uploader` DISABLE KEYS */;
-INSERT INTO `uploader` VALUES (10,'tvngoan','08aff88b292fe6eed1747cc2a08622f2ba1e39c2','ngoan98travinh@gmail.com','Ngoan Tran'),(11,'jackie','287667f9a218710dfbd7e6f58d64be83baecc4b2','jackie@mail.local','Jackie Chan'),(14,'joker','2fa570528a77894e2cc8cede9f0af2696a855e9b','joke@mail.co','Joker');
+INSERT INTO `uploader` VALUES (10,'tvngoan','08aff88b292fe6eed1747cc2a08622f2ba1e39c2','ngoan98travinh@gmail.com','Ngoan Tran'),(11,'jackie','287667f9a218710dfbd7e6f58d64be83baecc4b2','jackie@mail.local','Jackie Chan'),(14,'joker','2fa570528a77894e2cc8cede9f0af2696a855e9b','joke@mail.co','Joker'),(15,'user1','9b384a644e41a5c99e18f19526abaff8ac93c347','user1@local.co','user1');
 /*!40000 ALTER TABLE `uploader` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -112,4 +112,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-11-29  1:52:53
+-- Dump completed on 2019-12-01  9:25:39
